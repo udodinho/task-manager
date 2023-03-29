81019e88619b48ccb97c8f718fe56ace8034ec78
udodinho <udodinho200@gmail.com>
2023-03-29 09:02:24 -0700

    Add get single task and delete a task

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,5 +1,6 @@
 const Task = require("../models/tasks")
 const asyncWrapper = require("../middleware/async")
+const { createCustomError } = require("../errors/custom-error")
 
 const getAllTasks = asyncWrapper(async (req, res) => {
         const tasks = await Task.find({})
@@ -11,8 +12,29 @@ const createTask = asyncWrapper(async (req, res) => {
         res.status(201).json({ task })
 })
 
+const getTask = asyncWrapper(async (req, res, next) => {
+        const { id: taskID } = req.params
+        const task = await Task.findById({ _id: taskID })
+
+        if (!task) {
+            return next(createCustomError(404, `No task with the id ${taskID} `))
+        }
+
+        res.status(200).json({ task })
+})
+
+const deleteTask = asyncWrapper(async (req, res) => {
+        const { id: taskID } = req.params
+        const task = await Task.findByIdAndDelete({ _id: taskID })
+
+        if (!task) {
+            return next(createCustomError(404, `No task with the id ${taskID} `))
+        }
+
+        res.status(200).json({ task })
+})
 
 
 module.exports = {
-    getAllTasks, createTask
+    getAllTasks, createTask, getTask, updateTask, deleteTask
 }
